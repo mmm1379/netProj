c3d46d36c0fdde0c995f24c6ed54cc341d97047c
mmm1379 <mmmasoudpour@gmail.com>
2023-02-17 16:59:37 +0330

    some refactoring + implementations

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 from math import ceil
+from uuid import uuid4
 
 SOURCE_IP = "127.0.0.1"
 SOURCE_PORT = 5678
@@ -11,30 +12,31 @@ udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_socket.bind((SOURCE_IP, SOURCE_PORT))
 
 # prepare the payload
-samplePayload = ','.join(map(str,list(range(10000)))) # hello_world
+samplePayload = ','.join(map(str, list(range(10000))))  # hello_world
 
 
 # send out the packet
 def sendPayloadInSmallChunks(payload, destIP, destPort):
-    convertedChunks = convertChunk(payload, destIP, destPort)
+    id = uuid4().hex
+    convertedChunks = convertChunk(id, payload, destIP, destPort)
     for chunk in convertedChunks:
         udp_socket.sendto(str.encode(chunk), (DESTINATION_IP, DESTINATION_PORT))
 
 
-def convertChunk(payload, destIP, destPort):
+def convertChunk(id, payload, destIP, destPort):
     chunks = []
     maxChunkSize = 1000
     for i in range(ceil(len(payload) / maxChunkSize)):
         isLastChunk = 0
-        if i+1 == ceil(len(payload) / maxChunkSize):
+        if i + 1 == ceil(len(payload) / maxChunkSize):
             isLastChunk = 1
         payloadChunk = payload[i * maxChunkSize:(i + 1) * maxChunkSize]
-        chunks.append(f"{makeHeaderForChunk(isLastChunk, i, destIP, destPort)},{payloadChunk}")
-    return reversed(chunks)
+        chunks.append(f"{makeHeaderForChunk(id, isLastChunk, i, destIP, destPort)},{payloadChunk}")
+    return chunks
 
 
-def makeHeaderForChunk(isLastChunk, chunkNumber, destIP, destPort):
-    return f"{isLastChunk},{format(chunkNumber, '010d')},{destIP},{destPort}"
+def makeHeaderForChunk(id, isLastChunk, chunkNumber, destIP, destPort):
+    return f"{id},{isLastChunk},{format(chunkNumber, '010d')},{destIP},{destPort}"
 
 
-sendPayloadInSmallChunks(samplePayload, "192.168.1.1", "65000")
+sendPayloadInSmallChunks(samplePayload, destIP="192.168.1.1", destPort="65000")
--- xclient.py
+++ xclient.py
@@ -1,49 +1,144 @@
 import socket
+from math import ceil
+from queue import Queue
+from threading import Thread
+
+clientQueue = Queue(maxsize=100)
+responseQueue = Queue(maxsize=100)
 
 
-def listenForUDP():
-    IP = '0.0.0.0'  # Receive any incoming UDP packet on this port
-    PORT = 5679  # Example port
-    ADDRESS = (IP, PORT)
-    stack = []
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(ADDRESS)
-
-    def receiveMessage():
-        messageSize = 0
-        numberOfReceivedChunks = 0
-
-        def processReceivedChunk(chunk):
-            isLastChunk, chunkNumber, destIP, destPort, actualChunk = chunk.split(',', 4)
-            isLastChunk = int(isLastChunk)
-            chunkNumber = int(chunkNumber)
-            return isLastChunk, chunkNumber, destIP, destPort, actualChunk
-
-        while True:
-            data, address = s.recvfrom(4096)
-            processedChunk = processReceivedChunk(data.decode('utf-8'))
-            stack.append(processedChunk)
-            isLastChunk, chunkNumber = processedChunk[:2]
-
-            if isLastChunk:
-                messageSize = chunkNumber + 1
-                message = messageSize * [""]
-            numberOfReceivedChunks += 1
-            if messageSize:
-                while len(stack):
-                    isLastChunk, chunkNumber, destIP, destPort, actualChunk = stack.pop()
-                    message[chunkNumber] = actualChunk
-            if numberOfReceivedChunks == messageSize:
-                print(f"Received from {address}: , {''.join(message)}")
-                print(f"destination address: {destIP}, {destPort}")
-                return
-
-    while True:
-        receiveMessage()
+class Chunk:
+    def __init__(self, unprocessedChunk):
+        self.id, \
+        self.isLastChunk, \
+        self.chunkNumber, \
+        self.destIP, \
+        self.destPort, \
+        self.data = unprocessedChunk.split(',', 5)
+        self.isLastChunk = int(self.isLastChunk)
+        self.chunkNumber = int(self.chunkNumber)
 
 
-listenForUDP()
+class Message:
+    messages = {}
+
+    def __init__(self, firstReceivedChunk: Chunk, sourceAddress, id):
+        self.id = id
+        self.sourceIP = sourceAddress[0]
+        self.sourcePort = str(sourceAddress[1])
+        self.destIP = firstReceivedChunk.destIP
+        self.destPort = firstReceivedChunk.destPort
+        self.messageSize = 0
+        self.messageChunksInOrder = []
+        self.numberOfReceivedChunks = 0
+        self.stack = []
+
+    @staticmethod
+    def getMessageByProperties(chunk, address):
+        id = chunk.id
+        if id not in Message.messages:
+            Message.messages[id] = Message(chunk, address, id)
+        return Message.messages[id]
+
+    def addChunk(self, chunk: Chunk):
+        self.stack.append(chunk)
+        if chunk.isLastChunk:
+            self.messageSize = chunk.chunkNumber + 1
+            self.messageChunksInOrder = self.messageSize * [None]
+        self.numberOfReceivedChunks += 1
+
+        if self.messageSize:
+            while len(self.stack):
+                chunk = self.stack.pop()
+                self.messageChunksInOrder[chunk.chunkNumber] = chunk
+
+    def isComplete(self):
+        if self.numberOfReceivedChunks == self.messageSize:
+            return True
+        return False
+
+    def getWholeMessage(self):
+        return ''.join(
+            list(
+                map(
+                    lambda x: x.data,
+                    self.messageChunksInOrder
+                ))
+        )
+
+    def printMessageDescription(self):
+        print(f"Received from {self.sourceIP}:{self.sourcePort}: , {self.getWholeMessage()}")
+        print(f"destination address: {self.destIP}:{self.destPort}")
+
+
+xServerSocket = socket.socket()
+xServerIP = '127.0.0.1'
+xServerPort = 2005
 
 
 def establishConnectionToXServer():
-    pass
+    try:
+        xServerSocket.connect((xServerIP, xServerPort))
+    except socket.error as e:
+        print(str(e))
+
+
+def getFromClientAndSendToXServer():
+    XClientIP = '0.0.0.0'  # Receive any incoming UDP packet on this port
+    XClientPORT = 5679  # Example port
+    XClientADDRESS = (XClientIP, XClientPORT)
+
+    xClientUdpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    xClientUdpSocket.bind(XClientADDRESS)
+
+    def getEncapsulatedFromMessage(message):
+        wholeMessage = message.getWholeMessage()
+        return f"{len(wholeMessage)}, {message.id}, {message.destIP}, {message.destPort}, {wholeMessage}"
+
+    def sendMessageToXServer(message):
+        encapsulatedMessage = getEncapsulatedFromMessage(message)
+        xServerSocket.sendall(str.encode(encapsulatedMessage))
+
+    def receiveMessageFromClient():
+        while True:
+            data, address = xClientUdpSocket.recvfrom(4096)
+            chunk = Chunk(data.decode('utf-8'))
+            message = Message.getMessageByProperties(chunk, address)
+            message.addChunk(chunk)
+            if message.isComplete():
+                return message
+
+    while True:
+        receivedMessage = receiveMessageFromClient()
+        sendMessageToXServer(receivedMessage)
+        clientQueue.put(receivedMessage.id)
+        Message.messages.pop(receivedMessage.id)
+        receivedMessage.printMessageDescription()
+
+
+def receiveFromXServerAndSendToClient():
+    def getResponse():
+        firstMessage = xServerSocket.recv(2048).decode('utf-8')
+        size, res = firstMessage.split(',', 1)
+        size = int(size)
+        for i in range(ceil(size / 2048) - 1):
+            res += xServerSocket.recv(2048).decode('utf-8')
+        return res
+
+    def extractInfoFromResponse(response):
+        raise Exception("not implemented")
+        return "", ""
+        pass
+
+    while True:
+        response = getResponse()
+        messageId, data = extractInfoFromResponse(response)
+
+
+
+
+establishConnectionToXServer()
+t1 = Thread(target=getFromClientAndSendToXServer)
+t2 = Thread(target=receiveFromXServerAndSendToClient)
+t1.start()
+t2.start()
